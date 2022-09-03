8ea99c5253a69d7f75d1f846ee34b8ecb673fef7
Ricky428 <rickygill6760@gmail.com>
2022-09-03 11:27:46 +0530

    first backend assignment

--- src/index.js
+++ src/index.js
@@ -13,7 +13,127 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
 
+let oneMil = 1_000_000;
 
+function isNumericString(str) {
+    return !Number.isNaN(Number(str));
+}
+
+app.use((req,res,next) => {
+    const{num1,num2} = req.body;
+    if(num1===undefined || num2===undefined || num1==="" || num2==="") {
+        res.send({status : "error", message : "Invalid data types"});
+    }
+    else {
+        next();
+    }
+})
+
+app.use((req,res,next) => {
+    const{num1,num2} = req.body;
+    if(!isNumericString(num1) || !isNumericString(num2)) {
+        res.send({status : "error", message : "Invalid data types"});
+    }
+    else {
+        next();
+    }
+})
+
+app.use((req,res,next) => {
+    const{num1,num2} = req.body;
+    const actualNum1 = Number(num1);
+    const actualNum2 = Number(num2);
+    req.body.num1 = actualNum1;
+    req.body.num2 = actualNum2;
+    next();
+})
+
+app.use((req,res,next) => {
+    const{num1,num2} = req.body;
+   
+    if(num1 < -oneMil || num2 < -oneMil) {
+        res.send({status : "error", message : "Underflow"});
+    }
+    else if(num1 > oneMil || num2 > oneMil) {
+        res.send({status : "error", message : "Overflow"});
+    }
+    else {
+        next();
+    }
+})
+
+app.get('/', (req,res)=> {
+
+    res.send("Hello world!")
+})
+
+app.post('/add', (req,res,next)=> {
+    const{num1,num2} = req.body;
+    const sum = num1 + num2;
+
+    req._payload = {
+        status: "success",
+        message: "the sum of given two numbers",
+        sum,
+    }
+    next();
+})
+
+app.post('/sub', (req,res)=> {
+    const{num1,num2} = req.body;
+    const difference = num1 - num2;
+    
+    req._payload = {
+        status: "success",
+        message: "the difference of given two numbers",
+        difference,
+    }
+    next();
+})
+
+app.post('/multiply', (req,res,next)=> {
+    const{num1,num2} = req.body;
+    const result = num1 * num2;
+
+    req._payload = {
+        status: "success",
+        message: "The product of given numbers",
+        result,
+    }
+    next();
+})
+
+app.post('/divide', (req,res,next)=> {
+    const{num1,num2} = req.body;
+    
+    if(num2===0) {
+        res.send({status : "error", message : "Cannot divide by zero"});
+    }
+
+    const result = num1 / num2;
+
+    req._payload = {
+        status: "success",
+        message: "The division of given numbers",
+        result,
+    }
+    next();
+})
+
+app.use(() => {
+    let{sum,difference,result} = req._payload;
+    let value  = sum || difference || result;
+
+    if(value < -oneMil) {
+        res.send({status : "error", message : "Underflow"});
+    }
+    else if(value > oneMil) {
+        res.send({status : "error", message : "Overflow"});
+    }
+    else {
+        res.send(req_payload);
+    }
+})
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;
